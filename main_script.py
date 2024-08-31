8b1d42863929ad2b6e16ec3dc3d2d37d36d0f047
aayur <74551140+aayurdekate@users.noreply.github.com>
2024-08-31 11:15:27 +0530

    Delete main_script.py

--- main_script.py
+++
@@ -1,9 +0,0 @@
-# main_script.py
-
-from smartscan_registration_module import RegisterUserFromSmartScan
-
-# Define the path to your SmartScan Code image
-image_path = "path_to_your_smartscan_code_image.png"
-
-# Invoke the function to register user from the SmartScan Code
-RegisterUserFromSmartScan(image_path)
